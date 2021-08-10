bf74d714ef808cd11e5b20d69a46f0b5f50354d3
channajayanath.cj <channajayanath.cj@gmail.com>
2021-08-10 23:45:52 +0530

    q8

---
+++ Q8/myscript.py
@@ -0,0 +1,11 @@
+import sys
+import hashlib
+import string
+import secrets
+salt_length = 20
+alphabet = string.ascii_letters + string.digits
+salt = ''.join(secrets.choice(alphabet) for i in range(salt_length))
+password = str(sys.argv[1])
+salted_password = salt+password
+hash = hashlib.sha512(salted_password.encode())
+print(hash.hexdigest())
